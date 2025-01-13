e5bb2916e51b3a5ade85d82cdf788b04b5317828
dev123 <hyunho3445@gmail.com>
2025-01-13 17:25:19 +0900

    2025/01/13 - boardform - fileupload

--- h/src/main/java/lh/h/service/BoardServiceImpl.java
+++ h/src/main/java/lh/h/service/BoardServiceImpl.java
@@ -1,14 +1,23 @@
 package lh.h.service;
 
+import jakarta.validation.Valid;
 import lh.h.entity.Board;
 import lh.h.interfaces.BoardService;
 import lh.h.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardServiceImpl implements BoardService {
@@ -32,9 +41,24 @@ public class BoardServiceImpl implements BoardService {
                 .orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없습니다."));
     }
 
-    /* 게시글 저장 */
+    /* form write(file upload + write) */
+    /** file upload saveUrl**/
     @Override
-    public void save(Board board) {
+    public void saveFile(Board board, MultipartFile file) throws IOException {
+        String projectPath = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "static", "files").toString();
+
+        UUID uuid = UUID.randomUUID();
+
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath + File.separator + fileName);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+        board.setFilepath("/files/" + fileName);
+
+        //게시글 정보 저장
         boardRepository.save(board);
     }
 
@@ -47,12 +71,13 @@ public class BoardServiceImpl implements BoardService {
         boardRepository.delete(board);
     }
 
+    /* board list*/
     @Override
     public Page<Board> boardList(Pageable pageable) {
         return boardRepository.findAll(pageable);
     }
 
-    /* Paging board list*/
+    /* board search */
     @Override
     public Page<Board> boardSearchList(String searchKeyword, Pageable pageable) {
         if (searchKeyword == null || searchKeyword.isBlank()) {
@@ -60,11 +85,4 @@ public class BoardServiceImpl implements BoardService {
         }
         return boardRepository.findByTitleContaining(searchKeyword, pageable);
     }
-
-
-//    /* Board Search */
-//    @Override
-//    public Page<Board> searchBoard(String searchKeyword, Pageable pageable) {
-//        return boardRepository.findByTitle(searchKeyword, pageable);
-//    }
 }
