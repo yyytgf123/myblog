bdd9de63016b7dbdc335e349eaa351d50cf1d244
dev123 <hyunho3445@gmail.com>
2025-01-18 21:52:33 +0900

    2025/01/18 - loginpage login

--- h/src/main/java/lh/h/config/SecurityConfig.java
+++ h/src/main/java/lh/h/config/SecurityConfig.java
@@ -6,29 +6,32 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize ->
-                        authorize
-                                .anyRequest().permitAll() // 모든 요청 허용
-                )
-                .csrf(csrf ->
-                        csrf.disable() // CSRF 비활성화
-                )
-                .headers(headers ->
-                        headers
-                                .frameOptions(frameOptions ->
-                                        frameOptions.disable() // frameOptions 비활성화
-                                )
-                );
+        http.formLogin()
+                .loginPage("/user/signin")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email") // 로그인 폼에서 email 필드 사용
+                .passwordParameter("password") // 패스워드 필드
+                .failureUrl("/user/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true) // 세션 무효화
+                .deleteCookies("JSESSIONID"); // 쿠키 삭제
+
 
         return http.build();
     }
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- h/src/main/java/lh/h/entity/Member.java
+++ h/src/main/java/lh/h/entity/Member.java
@@ -20,4 +20,7 @@ public class Member {
 
     @Column(unique = true)
     private String email;
+
+    // login state
+    private boolean loggedin;
 }
